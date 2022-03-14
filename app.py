64fb1a1ca0513eb28c3c45d6b7a9fc242f95f91e
yzyyz1387 <1796031384@qq.com>
2022-03-14 19:51:40 +0800

    update

--- app.py
+++ app.py
@@ -1,52 +1,50 @@
 from flask import Flask, render_template, abort
 import re
-from random import *
-import time
+import random
 import img
 from wsgiref.simple_server import make_server
 
-
 app = Flask(__name__)
-bg=img.get_img()
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html',bg=bg), 404
+    bg = img.get_img()
+    return render_template('404.html', bg=bg), 404
+
 
 @app.route('/nolink')
 def no_page():
+    bg = img.get_img()
     return render_template('nolink.html', bg=bg)
+
 
 @app.route('/')
 def story():
+    bg = img.get_img()
     text = open("story", "r", errors='ignore', encoding='utf-8')
-    sotry = text.read()
+    story_ = text.read()
     findSum = re.compile('!story(.*?)story!', re.S)
-    sum = re.findall(findSum, sotry)
-    # 故事总数
+    sum_ = re.findall(findSum, story_)
     findInfo = re.compile(r'【(.*?)】', re.S)
-    # print(findInfo)
-    # print(len(sum))
-    number = randint(0, len(sum) - 1)
-    # print(number)
-    info = choice(sum)
+    number = random.randint(0, len(sum_) - 1)
+    info = random.choice(sum_)
     info = re.findall(findInfo, info)
     info[4] = info[4].replace("\n", "<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")
-    
-    return render_template("index.html", info=info,bg=bg)
+
+    return render_template("index.html", info=info, bg=bg)
     abort(404)
+
 
 @app.route('/about')
 def about():
-    text = open("story", "r", errors='ignore', encoding='utf-8')
-    sotry = text.read()
-    # print(sotry)
+    bg = img.get_img()
+    text = open("story_", "r", errors='ignore', encoding='utf-8')
+    story_ = text.read()
     findSum = re.compile('!story(.*?)story!', re.S)
-    sum = re.findall(findSum, sotry)
-    length=len(sum)
-    return render_template("about.html", length=length,bg=bg)
-
+    sum_ = re.findall(findSum, story_)
+    length = len(sum_)
+    return render_template("about.html", length=length, bg=bg)
 
 
 if __name__ == '__main__':
--- img.py
+++ img.py
@@ -1,18 +1,17 @@
-
 import requests
 import json
+
 
 # 可选国家: en-US, zh-CN, ja-JP, en-AU, en-UK, de-DE, en-NZ
 def get_img():
     url = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'
-    data1='https://cn.bing.com/'
-    html = requests.get(url).text   # 获取这个网页源码
+    data1 = 'https://cn.bing.com/'
+    html = requests.get(url).text  # 获取这个网页源码
     text = json.loads(html)
     data = text['images'][0]['url']
-    img = data1+ data
+    img = data1 + data
     return img
 
 
-
-get_img()
-
+if __name__ == '__main__':
+    get_img()
